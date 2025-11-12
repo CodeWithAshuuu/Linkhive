a67de4127e1262d22a0e35b308a75f6d43c68a1d
Ashutosh Mohanty <b522009@iiit-bh.ac.in>
2025-11-12 12:41:00 +0530

    last push

---
+++ LINKHIVE CODE/src/components/socials.tsx
@@ -0,0 +1,16 @@
+import { FaInstagram, FaSnapchat, FaXTwitter, FaGithub } from "react-icons/fa6";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { LiaTelegramPlane } from "react-icons/lia";
+import { AiOutlineDiscord } from "react-icons/ai";
+import { CiFacebook } from "react-icons/ci";
+
+export const iconMapping:{[key: string]: JSX.Element} = {
+    'LinkedIn': <SlSocialLinkedin size={32} className="fill-white"/>,
+    'Github': <FaGithub size={32} className="fill-white"/>,
+    'Instagram': <FaInstagram size={32} className="fill-white"/>,
+    'Telegram': <LiaTelegramPlane size={32} className="fill-white"/>,
+    'Snapchat': <FaSnapchat size={32} className="fill-white"/>,
+    'Twitter' : <FaXTwitter size={32} className="fill-white"/>,
+    'Discord' : <AiOutlineDiscord size={32} className="fill-white"/>,
+    'Facebook' : <CiFacebook size={32} className="fill-white"/>
+};
